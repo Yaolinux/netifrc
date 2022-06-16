d51795cf2e67979cc51c1d4b6e7beaca5d804870
Hugo B <Graknu@users.noreply.github.com>
2022-06-16 08:51:16 +0200

    Update functions.sh

--- sh/functions.sh
+++ sh/functions.sh
@@ -7,11 +7,11 @@ if [ x$RC_GOT_FUNCTIONS = xyes -o -n "$(command -v ebegin 2>/dev/null)" ]; then
 	:
 
 # Then check for the presence of functions.sh
-elif [ -f /lib/gentoo/functions.sh ]; then
-	. /lib/gentoo/functions.sh
+elif [ -f /lib/yaolinux/functions.sh ]; then
+	. /lib/yaolinux/functions.sh
 
 else
-	echo "/lib/gentoo/functions.sh not found. Exiting"
+	echo "/lib/yaolinux/functions.sh not found. Exiting"
 	exit 1
 fi
 
